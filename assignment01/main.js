1474b02338a03d770fcd1b5e7fbdea4a69695d2b
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-02-19 22:05:26 +0200

    story automation algorithm

---
+++ assignment01/main.js
@@ -0,0 +1,25 @@
+const mainCharacter = 'Alice';
+const gardeners = ['Two', 'Five', 'Seven'];
+const countGardeners = gardeners.length;
+const characters = {
+    women: 'Queen',
+    man: 'Knave of Hearts',
+    animal: 'White Rabbit',
+};
+
+document.body.innerHTML = `
+    <h2>Chapter VIII: The ${characters.women}’s Croquet-Ground</h2>
+    <p>A large rose-tree stood near the entrance of the garden: the roses growing on it were white, but there were ${countGardeners} gardeners at it, busily painting them red. ${mainCharacter} thought this a very curious thing, and she went nearer to watch them, and just as she came up to them she heard one of them say, “Look out now, ${gardeners[1]}! Don’t go splashing paint over me like that!”</p>
+    <p>“I couldn’t help it,” said ${gardeners[1]}, in a sulky tone; “${gardeners[2]} jogged my elbow.”</p>
+    <p>On which ${gardeners[2]} looked up and said, “That’s right, ${gardeners[1]}! Always lay the blame on others!”</p>
+    <p>“You’d better not talk!” said ${gardeners[1]}. “I heard the ${characters.women} say only yesterday you deserved to be beheaded!”</p>
+    <p>“What for?” said the one who had spoken first.</p>
+    <p>“That’s none of your business, ${gardeners[0]}!” said ${gardeners[2]}.</p>
+    <p>“Yes, it is his business!” said ${gardeners[1]}, “and I’ll tell him—it was for bringing the cook tulip-roots instead of onions.”</p>
+    <p>${gardeners[2]} flung down his brush, and had just begun “Well, of all the unjust things—” when his eye chanced to fall upon ${mainCharacter}, as she stood watching them, and he checked himself suddenly: the others looked round also, and all of them bowed low.</p>
+    <p>“Would you tell me,” said ${mainCharacter}, a little timidly, “why you are painting those roses?”</p>
+    <p>${gardeners[1]} and ${gardeners[2]} said nothing, but looked at ${gardeners[0]}. ${gardeners[0]} began in a low voice, “Why the fact is, you see, Miss, this here ought to have been a red rose-tree, and we put a white one in by mistake; and if the ${characters.women} was to find it out, we should all have our heads cut off, you know. So you see, Miss, we’re doing our best, afore she comes, to—” At this moment ${gardeners[1]}, who had been anxiously looking across the garden, called out “The ${characters.women}! The ${characters.women}!” and the ${countGardeners} gardeners instantly threw themselves flat upon their faces. There was a sound of many footsteps, and ${mainCharacter} looked round, eager to see the ${characters.women}.</p>
+    <p>First came ten soldiers carrying clubs; these were all shaped like the ${countGardeners} gardeners, oblong and flat, with their hands and feet at the corners: next the ten courtiers; these were ornamented all over with diamonds, and walked two and two, as the soldiers did. After these came the royal children; there were ten of them, and the little dears came jumping merrily along hand in hand, in couples: they were all ornamented with hearts. Next came the guests, mostly Kings and Queens, and among them ${mainCharacter} recognized the ${characters.animal}: it was talking in a hurried nervous manner, smiling at everything that was said, and went by without noticing her. Then followed the ${characters.man}, carrying the King’s crown on a crimson velvet cushion; and, last of all this grand procession, came THE KING AND QUEEN OF HEARTS.</p>
+    <p>${mainCharacter} was rather doubtful whether she ought not to lie down on her face like the ${countGardeners} gardeners, but she could not remember ever having heard of such a rule at processions; “and besides, what would be the use of a procession,” thought she, “if people had all to lie down upon their faces, so that they couldn’t see it?” So she stood still where she was, and waited.</p>
+    <p>When the procession came opposite to ${mainCharacter}, they all stopped and looked at her, and the ${characters.women} said severely “Who is this?” She said it to the ${characters.man}, who only bowed and smiled in reply.</p>
+`;
