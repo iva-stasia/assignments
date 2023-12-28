d61d783ff8ff796e00a076f10854fa6a2588d0c6
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-12-28 23:07:28 +0200

    complete practice 10

---
+++ ts-course/practice-10/src/index.ts
@@ -0,0 +1,30 @@
+// Вам потрібно створити тип DeepReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів.
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: DeepReadonly<T[K]>;
+};
+
+// Вам потрібно створити тип DeepRequireReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів та ще й робити їх обовʼязковими.
+
+type DeepRequireReadonly<T> = {
+  readonly [K in keyof T]-?: DeepRequireReadonly<T[K]>;
+};
+
+// Вам потрібно сворити тип UpperCaseKeys, який буде приводити всі ключи до верхнього регістру.
+
+type UpperCaseKeys<T> = {
+  [K in keyof T & string as Capitalize<K>]: T[K];
+};
+
+// Створіть тип ObjectToPropertyDescriptor, який перетворює звичайний обʼєкт на обʼєкт де кожне value є дескриптором.
+
+type Descriptor<T> = {
+  value: T;
+  writable: boolean;
+  enumerable: boolean;
+  configurable: boolean;
+};
+
+type ObjectToPropertyDescriptor<T> = {
+  [K in keyof T]: Descriptor<T[K]>;
+};
